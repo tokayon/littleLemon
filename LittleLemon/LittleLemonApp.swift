be3adbda3bf9fbb210447dca99a6021775d2e65b
Serge Sinkevych <serge@avicenaheart.com>
2026-05-03 14:58:52 -0700

    Initial Commit

---
+++ LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Serge Sinkevych on 5/3/26.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
